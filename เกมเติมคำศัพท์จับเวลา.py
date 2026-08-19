00cb9f3c114dee542f4e63bcaa29b5e3b53c1c51
402-13-bhumrachaya <54224@yupparaj.ac.th>
2026-08-19 15:21:33 +0700

    Update เกมเติมคำศัพท์จับเวลา.py

--- เกมเติมคำศัพท์จับเวลา.py
+++ เกมเติมคำศัพท์จับเวลา.py
@@ -40,7 +40,7 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
     else:
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
-        if u_ans3 == "guitar":
+    if u_ans3 == "guitar":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
